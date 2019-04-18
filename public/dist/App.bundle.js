c3a0f2b877d1e32f2396ddddf7bcd592a72494e6
Ryan Jones <ryandjones14@gmail.com>
2019-04-18 16:26:23 -0500

    added geolocation to store address

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -77,7 +77,20 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 function autocomplete(input, latInput, lngInput) {
-  console.log(input, latInput, lngInput);
+  if (!input) return;
+
+  var dropdown = new google.maps.places.Autocomplete(input);
+
+  dropdown.addListener('place_changed', function () {
+    var place = dropdown.getPlace();
+    console.log('place', place);
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+
+  input.on('keydown', function (e) {
+    if (e.keyCode === 13) e.preventDefault();
+  });
 };
 
 exports.default = autocomplete;
