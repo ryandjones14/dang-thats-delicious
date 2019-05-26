a0442761b93953dfda2e0723c41bd72ff9a4ff45
Ryan Jones <ryandjones14@gmail.com>
2019-05-26 17:38:18 -0500

    display stores tagged with tag or all stores on tags page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -78,7 +78,11 @@ exports.getStoreBySlug = async (req, res, next) => {
 };
 
 exports.getStoresByTag = async(req, res, next) => {
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, tag });
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({tags: tagQuery });
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  // res.json(result);
+  res.render('tag', { tags, tag, stores });
 };
