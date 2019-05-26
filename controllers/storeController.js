c8382834d4d8270e92cd84e925030fbafe80f30d
Ryan Jones <ryandjones14@gmail.com>
2019-05-26 17:26:20 -0500

    add ability to see store count by tag

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -76,3 +76,9 @@ exports.getStoreBySlug = async (req, res, next) => {
   if (!store) return next();
   res.render('store', {title: store.name, store});
 };
+
+exports.getStoresByTag = async(req, res, next) => {
+  const tags = await Store.getTagsList();
+  const tag = req.params.tag;
+  res.render('tag', { tags, tag });
+};
